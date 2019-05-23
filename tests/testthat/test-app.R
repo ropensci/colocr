1c12ffd2df17c118596a13541e75231c1c3c3ea9
MahShaaban <mahmoud.s.fahmy@students.kasralainy.edu.eg>
2019-05-23 19:54:30 +0900

    skip app tests on CRAN

--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -1,6 +1,8 @@
 context('app')
 
 test_that("app works", {
+  skip_on_cran()
+
   library(shinytest)
   app_dir <- system.file('colocr_app/', package = 'colocr')
   expect_pass(testApp(app_dir, compareImages = FALSE))
